abc80fc59d19b5b26c17cc1346ae5a2f76ab45af
unknown <mayurpawar8691@gmail.com>
2022-04-26 13:56:29 +0530

    Added: Ability to create contact

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -1,9 +1,28 @@
-/** printing welcome message **/
+import java.util.Scanner;
+import java.util.*;
 
-class AddressBookSystem
-{
-	public static void main(String args[])
-	{
+class AddressBookSystem {
+	public static void main(String args[]) {
 		System.out.println("Welcome To AddressBook !");
+		Contacts.createContact("Mayur", "Sector-34", "Panvel", "Maharashtra", "410209", "9067679078", "opsdsnmn@gmail.com");
+	}
+}
+
+class Contacts {
+	/**creating contact using Arraylist **/
+
+	static void createContact(String name, String address, String city, String state, String zipCode, String phoneNo, String email) {
+		ArrayList<String> firstContact = new ArrayList<String>(7);
+				
+		firstContact.add(name);
+		firstContact.add(address);
+		firstContact.add(city);
+		firstContact.add(state);
+		firstContact.add(zipCode);
+		firstContact.add(phoneNo);
+		firstContact.add(email);
+			
+		System.out.println(firstContact );
+		
 	}
 }
